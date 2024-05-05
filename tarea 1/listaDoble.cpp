84975c0df94f7fe8dfaf786bfe20e1456c352bff
William Pineda <00225919@uca.edu.sv>
2024-05-05 17:36:10 -0600

    add: lista Doblemente enlazada

---
+++ tarea 1/listaDoble.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+class Nodo
+{
+public:
+    int Valor;
+    Nodo *Siguiente;
+    Nodo *Anterior;
+};
+
+void imprimirLista(Nodo *n)
+{
+    while (n != nullptr)
+    {
+        cout << n->Valor << endl;
+        n = n->Siguiente;
+    }
+}
+
+void insertarAlFrente(Nodo **head, int nuevoValor)
+{
+    // pasos para insertar nuevo valor al frente de la lista
+    //  1- Preparar un nuevo nodo
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->Valor = nuevoValor;
+    // 2- Ponerlo al frente de la lista
+    nuevoNodo->Siguiente = *head;
+    nuevoNodo->Anterior = nullptr; // Como el nuevo nodo será el primero, su anterior es nulo
+
+    // 3- Verificamos que La lista no esta vacia ajustamos el nodo anterior al acutal
+    if (*head != nullptr)
+        (*head)->Anterior = nuevoNodo;
+
+    // 3- Mover la cabeza de la lista al nuevo nodo
+    *head = nuevoNodo;
+}
+
+void insertarAlFinal(Nodo **head, int nuevoValor)
+{
+    // 1-preparar un nuevo nodo
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->Valor = nuevoValor;
+    nuevoNodo->Siguiente = nullptr;
+    // 2-si la lista esta vacia, el nuevo nodo sera la cabeza de la lista
+    if (*head == nullptr)
+    {
+        nuevoNodo->Anterior = nullptr; // si es encenrtra vacia el nuevo nodo no tiene , un nodo anterior
+        *head = nuevoNodo;
+        return;
+    }
+    // 3- encontrar el último nodo
+    Nodo *ultimo = *head;
+    while (ultimo->Siguiente != nullptr)
+    {
+        ultimo = ultimo->Siguiente;
+    }
+    // 4. es aqui donde realmente enlazamos las listas, y procedemos a enlazar los nosdos nuevos y anteriores
+    ultimo->Siguiente = nuevoNodo;
+    nuevoNodo->Anterior = ultimo;
+}
+
+void insertarEnPosicion(Nodo *anterior, int nuevoValor)
+{
+    // 1- verificar si el nodo anterior es NULL
+    if (anterior == nullptr)
+    {
+        cout << "El nodo anterior no puede ser NULL" << endl;
+        return;
+    }
+    // 2- preparar el nuevo nodo
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->Valor = nuevoValor;
+    nuevoNodo->Siguiente = anterior->Siguiente; // Establece el siguiente del nuevo nodo como el siguiente del nodo anterior
+    nuevoNodo->Anterior = anterior;             // Establece el anterior del nuevo nodo como el nodo anterior proporcionado
+    // 3- si el nodo anterior tiene un siguiente nodo, ajustar el puntero anterior del siguiente nodo
+    if (anterior->Siguiente != nullptr)
+    {
+        anterior->Siguiente->Anterior = nuevoNodo; // El nodo siguiente al nodo anterior ahora tendrá como anterior al nuevo nodo
+    }
+    // 4- digamos entonces que enlazamos el nuevo nodo después del nodo anterior
+    anterior->Siguiente = nuevoNodo;
+}
+
+int main()
+{
+    Nodo *head = nullptr;
+
+    insertarAlFrente(&head, 3);
+    insertarAlFrente(&head, 2);
+    insertarAlFrente(&head, 1);
+
+    insertarAlFinal(&head, 4);
+
+    Nodo *nodoActual = head;
+    while (nodoActual != nullptr && nodoActual->Valor != 3)
+    {
+        nodoActual = nodoActual->Siguiente;
+    }
+    if (nodoActual != nullptr)
+    {
+        insertarEnPosicion(nodoActual, 7);
+    }
+
+    cout << "Lista hacia adelante:" << endl;
+    imprimirLista(head);
+    cout << "Lista hacia atras:" << endl;
+    Nodo *ultimo = head;
+
+    // verifica recoririendo de la lista avanzando hacia el último nodo
+    while (ultimo->Siguiente != nullptr)
+    {
+        ultimo = ultimo->Siguiente;
+    }
+    // verifica recoririendo hacia atrás a través de la lista, imprimiendo los valores de los nodos en orden inverso
+    while (ultimo != nullptr)
+    {
+        cout << ultimo->Valor << endl;
+        ultimo = ultimo->Anterior;
+    }
+
+    return 0;
+}
